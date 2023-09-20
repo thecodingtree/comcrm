7206e6fb430f88fde546c4a39503125e730cfe80
David Mitchell <dmitchek@gmail.com>
2023-09-20 10:10:57 -0400

    Added authjs to project
    - Added authjs to project for all authentication
    - Started with GitHub as initial auth provider
    - Create /dashboard route to show protected routes
    - Setup initial graphql auth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,25 @@
+import { type NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  debug: true,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+};
