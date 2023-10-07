342231ac47d778d1270477f90be266608b832190
David Mitchell <dmitchek@gmail.com>
2023-10-07 10:47:15 -0400

    Add Apollo Client to project
    This used the experimental apollo library for using ApolloClient in Nextjs 13+ (i.e. RSC, SSR)

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -2,12 +2,28 @@ import { getCompaniesForUser } from '@/actions/entity';
 
 import CompaniesTable from '@/components/tables/CompaniesTable';
 
+import { getClient } from '@/lib/client';
+
+import { gql } from '@apollo/client';
+
+const query = gql`
+  query {
+    companies {
+      id
+      name
+    }
+  }
+`;
+
 export default async function Companies() {
   const companies = await getCompaniesForUser();
+
+  const { data } = await getClient().query({ query });
 
   return (
     <div>
       <h1>{'Companies!'}</h1>
+      <div>{JSON.stringify(data)}</div>
       <CompaniesTable companies={companies} />
     </div>
   );
