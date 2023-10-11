92d1f961733b1b8f596026fc91c3dcf93246fa82
David Mitchell <dmitchek@gmail.com>
2023-10-11 17:02:03 -0400

    Added all pages for the Companies stack
    This includes the list view of all companies and the details view of a single company.

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -4,15 +4,15 @@ import { getServerSession } from 'next-auth';
 
 import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 import { getClient } from '@/lib/client';
-import { GET_COMPANIES_FOR_USER } from '@/graphql/queries';
+import { GET_COMPANIES } from '@/graphql/queries';
 
 import CompaniesTable from '@/components/tables/CompaniesTable';
 
 export default async function Companies() {
   const session = await getServerSession(authOptions);
   const { data, loading, error } = await getClient().query({
-    query: GET_COMPANIES_FOR_USER,
-    variables: { user: session?.user?.id },
+    query: GET_COMPANIES,
+    variables: { filter: { user: session?.user?.id } },
   });
 
   return (
