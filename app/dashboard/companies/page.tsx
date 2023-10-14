0cb8aad8e0b2654a45486e0f4807ed07873b348a
David Mitchell <dmitchek@gmail.com>
2023-10-14 19:01:43 -0400

    Added Properties Page
    Added properties page and also fixed some issues with how data was being loaded and refreshed after adding a new entity.

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -1,25 +1,10 @@
-import Link from 'next/link';
-
 import { getServerSession } from 'next-auth';
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/options';
-import { getClient } from '@/lib/client';
-import { GET_COMPANIES } from '@/graphql/queries';
-
-import CompaniesTable from '@/components/tables/CompaniesTable';
+import CompanyPage from '@/components/company/CompaniesPage';
 
 export default async function Companies() {
-  const session = await getServerSession(authOptions);
-  const { data, loading, error } = await getClient().query({
-    query: GET_COMPANIES,
-    variables: { filter: { user: session?.user?.id } },
-  });
+  // TODO: don't love this but it works for now
+  const session = await getServerSession();
 
-  return (
-    <div>
-      <h1>{'Companies!'}</h1>
-      <Link href="/dashboard/companies/new">{'Add New'}</Link>
-      <CompaniesTable companies={data?.companies} />
-    </div>
-  );
+  return <CompanyPage user={session?.user} />;
 }
