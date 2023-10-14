f0c393edfdfe9ae7b23fef47d6f399142e09fe46
David Mitchell <dmitchek@gmail.com>
2023-10-14 19:59:35 -0400

    Added Contacts Page

---
+++ components/contact/ContactAdd.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useMutation } from '@apollo/client';
+
+import { useSession } from 'next-auth/react';
+
+import { useDisclosure } from '@mantine/hooks';
+import { Stack, Button, Modal } from '@mantine/core';
+
+import { ADD_CONTACT } from '@/graphql/mutations';
+import { GET_CONTACTS } from '@/graphql/queries';
+
+import ContactForm from './form/ContactForm';
+
+export default function ContactAdd() {
+  const { data: session, status } = useSession();
+  const [addContact, { data, loading, error }] = useMutation(ADD_CONTACT, {
+    onCompleted: () => {
+      close();
+    },
+    refetchQueries: [GET_CONTACTS],
+  });
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const submitHandler = (values: any) => {
+    addContact({
+      variables: {
+        name: values.name,
+        surName: values.surName,
+        address: {
+          street: values.street,
+          city: values.city,
+          state: values.state,
+          zip: values.zip,
+        },
+        user: session?.user?.id ?? '',
+      },
+    });
+  };
+
+  return (
+    <Stack align="center">
+      <Button onClick={open} w={400}>
+        Add Contact
+      </Button>
+      <Modal opened={opened} onClose={close} size="lg" centered>
+        <ContactForm onSubmit={submitHandler} submitting={loading} />
+      </Modal>
+    </Stack>
+  );
+}
---
+++ components/contact/ContactsTable.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { Table, Text, Space } from '@mantine/core';
+import Link from 'next/link';
+import { Contact } from '@/generated/resolvers-types';
+
+import { EntitiesTable, ETColumn } from '../entities/EntitiesTable';
+
+interface ContactsTableProps {
+  contacts?: Contact[];
+  onDeleteContact?(id: string): void;
+}
+
+export default function ContactsTable({
+  contacts,
+  onDeleteContact,
+}: ContactsTableProps) {
+  const columns = [
+    {
+      name: 'First Name',
+      key: 'name',
+      sortable: true,
+    },
+    {
+      name: 'Last Name',
+      key: 'surName',
+      sortable: true,
+    },
+    {
+      name: 'Address',
+      key: 'address',
+      sortable: false,
+    },
+    {
+      name: 'Actions',
+      key: 'actions',
+      sortable: false,
+    },
+  ] as ETColumn[];
+
+  const deleteBtn = (id: string) => {
+    return (
+      <button
+        onClick={() => {
+          onDeleteContact && onDeleteContact(id);
+        }}
+      >
+        Delete
+      </button>
+    );
+  };
+
+  const goToContact = (id: string) => {
+    return (
+      <Link href={`/dashboard/contacts/${id}`}>
+        <button>Go to</button>
+      </Link>
+    );
+  };
+
+  const rowRenderer = (row: Contact) => {
+    if (row) {
+      const addressStr = `${row?.address?.street} ${row?.address?.city} ${row?.address?.state} ${row?.address?.zip}`;
+      return (
+        <Table.Tr key={row.name}>
+          <Table.Td>{row.name}</Table.Td>
+          <Table.Td>{row.surName}</Table.Td>
+          <Table.Td>{addressStr}</Table.Td>
+          <Table.Td>
+            <div>{deleteBtn(row.id)}</div>
+            <div>{goToContact(row.id)}</div>
+          </Table.Td>
+        </Table.Tr>
+      );
+    } else {
+      return (
+        <Table.Tr key="empty">
+          <Table.Td colSpan={columns.length}>
+            <Text ta="center">{'No Contacts'}</Text>
+            <Space h="lg" />
+          </Table.Td>
+        </Table.Tr>
+      );
+    }
+  };
+
+  return (
+    <EntitiesTable
+      entities={contacts}
+      columns={columns}
+      rowRenderer={rowRenderer}
+    />
+  );
+}
