0e42475607060f3fb6a8556f58d42599e9b24a00
David Mitchell <dmitchek@gmail.com>
2023-10-14 20:10:58 -0400

    Added initial Contact Details page

---
+++ components/contact/ContactDetail.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useQuery } from '@apollo/client';
+import { GET_CONTACT } from '@/graphql/queries';
+
+import { Grid } from '@mantine/core';
+
+import ContactInfo from './ContactInfo';
+import ContactNotes from './ContactNotes';
+
+export default function ContactDetails() {
+  const params = useParams();
+
+  const { data, loading, error } = useQuery(GET_CONTACT, {
+    variables: { id: params?.id },
+  });
+
+  return (
+    <Grid>
+      <Grid.Col span={{ base: 12, lg: 6 }}>
+        <ContactInfo
+          name={data?.contact?.name}
+          surName={data?.contact?.surName}
+          address={data?.contact?.address}
+        />
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, lg: 6 }}>
+        <ContactNotes />
+      </Grid.Col>
+    </Grid>
+  );
+}
