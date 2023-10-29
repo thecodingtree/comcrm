e9d34eb14ec4162a671c3b5169240e89a6511668
David Mitchell <dmitchek@gmail.com>
2023-10-29 09:51:15 -0400

    Initial TRPC implementation
    This implementation was copied from 'create-t3-app'.
    
    Also cleaned up a few things including multiple instanciations of PrismaClient

--- lib/apollo-provider.tsx
+++ lib/apollo-provider.tsx
@@ -10,7 +10,7 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+    uri: `${process.env.NEXT_PUBLIC_URL}/api/graphql`,
   });
 
   return new NextSSRApolloClient({
