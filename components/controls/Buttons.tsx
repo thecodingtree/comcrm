20f0acbcb51166231d0bfbc3ac38cb2a012c081a
David Mitchell <dmitchek@gmail.com>
2024-01-18 15:29:19 -0500

    Replaced TextInput and Loader in EntityAutocomplete component

--- components/controls/Buttons.tsx
+++ components/controls/Buttons.tsx
@@ -1,17 +1,21 @@
 import { Button } from '@/components/ui/button';
 import { IconPencil, IconX, IconCheck } from '@tabler/icons-react';
 
+import { cn } from '@/lib/utils';
+
 export function IconButton({
+  className,
   onClick,
   icon,
 }: {
+  className?: string;
   onClick?: () => void;
   icon: React.ReactNode;
 }) {
   return (
     <Button
       variant="ghost"
-      className="p-0 hover:bg-transparent"
+      className={cn('p-0 hover:bg-transparent', className)}
       onClick={onClick}
     >
       {icon}
