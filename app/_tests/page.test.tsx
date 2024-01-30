edcdf12857ebe07e22803014ed3441237c0494b8
David Mitchell <dmitchek@gmail.com>
2024-01-30 14:00:44 -0500

    Vitests initial config

---
+++ app/_tests/page.test.tsx
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import HomePage from '../page';
+
+test('Page', () => {
+  render(<HomePage />);
+  expect(screen.findByText('Home Page')).toBeDefined();
+});
