6dd802d336115c8159bd21f456286dde948a9f41
David Mitchell <dmitchek@gmail.com>
2024-02-12 12:48:53 -0500

    Added Tasks trpc call

---
+++ components/tasks/TaskListSkeleton.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function TaskListSkeleton() {
+  return (
+    <div className="flex items-center space-x-4">
+      <Skeleton className="h-10 w-10 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[150px]" />
+        <Skeleton className="h-4 w-[100px]" />
+      </div>
+    </div>
+  );
+}
