104afcd2da863d9fd7f3fae34ba7cc51b43af095
David Mitchell <dmitchek@gmail.com>
2024-02-22 13:15:59 -0500

    Added actions checkbox to task items
    This allows the user to select one or more tasks and mark them as completed

--- components/tasks/TaskItem.tsx
+++ components/tasks/TaskItem.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import { IconCalendar } from '@tabler/icons-react';
 
+import { Checkbox } from '@/components/ui/checkbox';
 import { CardContent, CardHeader, Card } from '@/components/ui/card';
 import {
   Collapsible,
@@ -13,45 +14,50 @@ import {
 
 import { IconExpand, IconCollapse, IconTime } from '@/components/common/icons';
 
-import { TaskType } from '@/server/sharedTypes';
+import { TaskResult } from '@/server/task';
 
-import { getTaskIcon, getTaskDateLabel } from './utils';
+import { getTaskIcon, getTaskDateLabel, getPrioirtyBadge } from './utils';
 
 export default function TaskItem({
-  type,
-  startDate,
-  endDate,
-  description,
-  content,
-  assignedBy,
+  task,
+  selected,
+  onSelectChange,
 }: {
-  type: TaskType;
-  startDate?: Date;
-  endDate: Date;
-  description: string;
-  content?: string;
-  assignedBy?: string;
+  task: TaskResult;
+  selected?: boolean;
+  onSelectChange?: (selected: boolean) => void;
 }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <Collapsible defaultOpen={false} onOpenChange={setIsExpanded}>
       <Card>
-        <CollapsibleTrigger className="w-full">
-          <CardHeader className="p-0 text-sm font-medium">
+        <CollapsibleTrigger className="w-full" asChild>
+          <CardHeader className="p-0 text-sm font-medium cursor-pointer">
             <div className="flex items-center justify-between px-4 py-2 bg-gray-100 dark:bg-gray-800/40 rounded-tl-lg rounded-tr-lg">
               <div>
                 <div className="flex items-center gap-2 ">
-                  {getTaskIcon(type)}
-                  <span className="">{description}</span>
+                  <div className="flex mr-2">
+                    <Checkbox
+                      checked={selected}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        onSelectChange && onSelectChange(!selected);
+                      }}
+                    />
+                  </div>
+                  {getTaskIcon(task.type)}
+                  <span className="">{task.description}</span>
                   <div className="font-light">
-                    {assignedBy ? `Assigned By: ${assignedBy}` : null}
+                    {task?.assignee?.name
+                      ? `Assigned By: ${task?.assignee?.name}`
+                      : null}
                   </div>
                 </div>
               </div>
 
               <div className="flex flex-row gap-4 items-center">
-                <div>{getTaskDateLabel(endDate, startDate)}</div>
+                <div>{getTaskDateLabel(task.endDate, task.startDate)}</div>
                 {isExpanded ? (
                   <IconCollapse className="w-4 h-4" />
                 ) : (
@@ -64,15 +70,18 @@ export default function TaskItem({
         <CollapsibleContent>
           <CardContent className="p-4">
             <div className="grid gap-4 text-gray-500 font-medium">
-              <div className="">{content}</div>
+              <div className="">{task.content}</div>
               <div className="grid gap-2 text-xs  dark:text-gray-400">
                 <div className="flex items-center gap-2">
                   <IconTime className="w-4 h-4" />
-                  <span className="">{endDate.toLocaleTimeString()}</span>
+                  <span className="">{task.endDate.toLocaleTimeString()}</span>
                 </div>
                 <div className="flex items-center gap-2">
                   <IconCalendar className="w-4 h-4" />
-                  <span className="">{endDate.toLocaleDateString()}</span>
+                  <span className="">{task.endDate.toLocaleDateString()}</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  {getPrioirtyBadge(task.priority)}
                 </div>
               </div>
             </div>
