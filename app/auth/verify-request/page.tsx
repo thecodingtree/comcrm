f7aa039b662a6670067414f24126d0790d252994
David Mitchell <dmitchek@gmail.com>
2024-03-09 10:54:59 -0500

    Custom Sign In Page Implementation
    Override Sign In page provided by next-auth

---
+++ app/auth/verify-request/page.tsx
@@ -0,0 +1,10 @@
+export default function VerifyRequest() {
+  return (
+    <>
+      <div className="text-xl text-center">Check your email!</div>
+      <div className="text-lg text-center">
+        A sign in link has been sent to your email address.
+      </div>
+    </>
+  );
+}
