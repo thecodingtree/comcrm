fefb397e8ade6d7b5e41a7732c8c5637da185371
David Mitchell <dmitchek@gmail.com>
2024-03-09 10:54:59 -0500

    Custom Sign In Page Implementation
    Override Sign In page provided by next-auth

---
+++ app/contacts/layout.tsx
@@ -0,0 +1,5 @@
+import MainLayout from '@/components/layouts/MainLayout';
+
+export default function ContactsLayout({ children }: { children: any }) {
+  return <MainLayout>{children}</MainLayout>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
   ],
   prefix: '',
   theme: {
